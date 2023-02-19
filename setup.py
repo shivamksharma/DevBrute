0a1d6c5a880290f27f23c15ad3e51cc5f08d94ce
Anonymous <shivam.s@gmail.com>
2023-02-19 17:28:51 +0530

    Fixing

--- setup.py
+++ setup.py
@@ -2,36 +2,31 @@ import os
 from subprocess import call
 
 os.system("sudo apt install python3-pip && sudo apt install tor")
-os.system("sudo pip3 install -U selenium pyvirtualdisplay pysocks mechanize xvfb argparse requests")
-
+os.system("pip3 install -U selenium pyvirtualdisplay pysocks mechanize xvfb argparse requests")
 
 os.system('firefox -v > tmp')
-result = open('tmp', 'r').read()
-marker = result.find('Firefox')+8
-version = result[marker:].splitlines()[0]
-a,b,c = version.split('.')
+with open('tmp', 'r') as f:
+    marker = f.read().find('Firefox')+8
+    version = f.read()[marker:].splitlines()[0]
 os.remove('tmp')
-
+a,b,c = version.split('.')
 FirefoxVersion = int(a)
-second = 0
 
 if FirefoxVersion < 53:
-    
-    first = 16
-    second = 1
+    call(["wget", "https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz"])
     os_bit = 64
 
 elif FirefoxVersion == 53 or FirefoxVersion == 54:
-    
-    first = 18
+    call(["wget", "https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz"])
+    os_bit = 64
 
 elif FirefoxVersion > 54:
-        
-        first = 19
+    call(["wget", "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz"])
+    os_bit = 64
 
-os.system("wget https://github.com/mozilla/geckodriver/releases/download/v0.{}.{}/geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,first,second,OS_bit))
-os.system("tar -xvzf geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
-os.system("rm geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,OS_bit))
+call(["tar", "-xvzf", "geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,os_bit)])
+os.remove("geckodriver-v0.{}.{}-linux{}.tar.gz".format(first,second,os_bit))
 os.system("chmod +x geckodriver")
 os.system("mv geckodriver /usr/local/bin/")
-chmod +x setup.py
+os.system("chmod +x setup.py")
+
