a8e38819b87a1194c547990edcd56ad90d81fe8d
Krish Kumar <krishkumar2306@gmail.com>
2025-02-07 23:03:57 +0530

    chores: fixing and updating

--- setup.py
+++ setup.py
@@ -1,38 +1,16 @@
 import os
-from subprocess import call
 
-# Install required packages
-os.system("sudo apt install python3-pip && sudo apt install tor")
-os.system("pip3 install -U selenium pyvirtualdisplay pysocks mechanize xvfb argparse requests proxyscrape")
+def install_requirements():
+    print("[*] Installing required Python packages...")
+    exit_code = os.system("pip3 install -r requirements.txt")
+    return exit_code == 0
 
-# Check Firefox version and download corresponding geckodriver
-os.system('firefox -v > tmp')
-with open('tmp', 'r') as f:
-    version_info = f.read()
-marker = version_info.find('Firefox') + 8
-version = version_info[marker:].splitlines()[0]
-os.remove('tmp')
-a, b, c = version.split('.')
-FirefoxVersion = int(a)
+def main():
+    print("DevBrute Setup\n")
+    if not install_requirements():
+        print("\n[!] Failed to install Python packages")
+        exit(1)
+    print("\n[+] Setup completed successfully!")
 
-if FirefoxVersion < 53:
-    call(["wget", "https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz"])
-    os_bit = 64
-elif 53 <= FirefoxVersion <= 54:
-    call(["wget", "https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz"])
-    os_bit = 64
-elif FirefoxVersion > 54:
-    call(["wget", "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz"])
-    os_bit = 64
-
-call(["tar", "-xvzf", "geckodriver-v0.{}.{}-linux{}.tar.gz".format(a, b, os_bit)])
-os.remove("geckodriver-v0.{}.{}-linux{}.tar.gz".format(a, b, os_bit))
-os.system("chmod +x geckodriver")
-os.system("mv geckodriver /usr/local/bin/")
-
-# Provide instructions for running the BruteForce Framework
-print("\nSetup complete!\n")
-print("To run the BruteForce Framework, use the following command:")
-print("python3 bruteforce.py -s <service> -u <username> -w <wordlist> -d <delay>\n")
-print("Example:")
-print("python3 bruteforce.py -s facebook -u username -w wordlist.txt -d 2\n")
+if __name__ == "__main__":
+    main()
